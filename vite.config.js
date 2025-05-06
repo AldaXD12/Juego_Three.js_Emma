5a7c4ba3385fb1ee6f75a1cca3df4854c84f3c0c
AldaXD12 <aldahirchin@gmail.com>
2025-05-05 21:17:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: 'Juego_Three.js_Emma',
   plugins: [react()],
-  base: './', // Muy importante para que funcione correctamente en producción (por ejemplo en GitHub Pages)
-  build: {
-    outDir: 'dist',
-  },
 })
