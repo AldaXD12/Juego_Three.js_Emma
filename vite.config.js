b92726986c20e6200981977673cb27ffeaa2476d
AldaXD12 <aldahirchin@gmail.com>
2025-05-05 23:06:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import autoModel from './models/auto.glb'
 loader.load(autoModel)
+loader.load('models/auto.glb') // relativo a /public
 
 export default defineConfig({
   base: 'Juego_Three.js_Emma',
